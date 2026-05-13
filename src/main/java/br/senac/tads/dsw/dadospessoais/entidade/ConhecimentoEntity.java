6812b21beed1b76c1fbdd4c0024b5165871cf38e
Lucasvg90 <lucastomsaw@gmail.com>
2026-05-13 14:52:38 -0300

    adição de Spring JWT e Security

---
+++ src/main/java/br/senac/tads/dsw/dadospessoais/entidade/ConhecimentoEntity.java
@@ -0,0 +1,42 @@
+package br.senac.tads.dsw.dadospessoais.entidade;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// =====================================================================
+// LEMBRE-SE: gere os getters, setters e o construtor sem argumentos
+// (obrigatório no JPA) para todos os campos desta classe.
+// =====================================================================
+
+@Entity
+@Table(name = "tb_conhecimentos")
+public class ConhecimentoEntity {
+    public ConhecimentoEntity() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true, length = 100)
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
